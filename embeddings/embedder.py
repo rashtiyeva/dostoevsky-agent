2e20ec6368f7ffeb1d37c9b9d39a21c7aa0fdb91
rashtiyeva <rashtiyevashafiga@gmail.com>
2026-09-22 18:47:02 +0400

    feat: configure embedding batch size

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
 MODEL_NAME = "BAAI/bge-m3"
+EMBEDDING_BATCH_SIZE = 32
 
 
 class EmbeddingModel:
@@ -8,19 +9,20 @@ class EmbeddingModel:
         self.model = SentenceTransformer(MODEL_NAME)
 
     def embed(self, text: str) -> list[float]:
-           embedding = self.model.encode(
+        embedding = self.model.encode(
             text,
             normalize_embeddings=True,
         )
-           return embedding.tolist()
-    
+        return embedding.tolist()
+
     def embed_batch(
         self,
         texts: list[str],
     ) -> list[list[float]]:
         embeddings = self.model.encode(
             texts,
+            batch_size=EMBEDDING_BATCH_SIZE,
             normalize_embeddings=True,
+            show_progress_bar=False,
         )
-
         return embeddings.tolist()
